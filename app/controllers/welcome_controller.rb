331bc654dd34212e70552ea20aa0cc3b21041aaf
Marlon J. Frausto <marlonjfrausto@gmail.com>
2016-02-09 15:51:18 -0800

    FEATURE: added the thumbnails for services to be based on an actual Service model

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+  	@services = Service.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+services_list = [
+  { "Title" => 'Service 1', 'Description' => 'Some details on Service 1', 'Image' => 'image1.jpg' },
+  { "Title" => 'Service 2', 'Description' => 'Some details on Service 2', 'Image' => 'image2.jpg' },
+  { "Title" => 'Service 3', 'Description' => 'Some details on Service 3', 'Image' => 'image3.jpg' },
+  { "Title" => 'Service 4', 'Description' => 'Some details on Service 4', 'Image' => 'image4.jpg'}
+]
+
+
+
+
+
+services_list.each do |service|
+  new_service = Service.new( title: service['Title'], description: service['Description'], service_img_link: service['Image'] )
+  if new_service.save
+	  puts new_service
+	 end
+end
