7cef67fa68e21b99160af55c58f4217e5f4f7b23
Marlon J. Frausto <marlonjfrausto@gmail.com>
2016-02-11 21:29:40 -0800

    removed the initializer

--- config/initializers/sparkpost_rails.rb
+++
@@ -1,7 +0,0 @@
-SparkpostRails.configure do |c|
-  c.api_key = '7683b93d766bdeace1ee925d8546746390841bd0'
-  c.track_opens = true
-  c.track_clicks = true
-  c.return_path = 'marlon@affordablehousingapp.com'
-  # c.campaign_id = 'YOUR-CAMPAIGN'
-end
