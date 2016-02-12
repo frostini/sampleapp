4d3edce003b8318d9a11b376856b2e0528eaa7a8
Marlon J. Frausto <marlonjfrausto@gmail.com>
2016-02-11 22:00:30 -0800

    testing

--- config/initializers/email_settings.rb
+++ config/initializers/email_settings.rb
@@ -33,9 +33,10 @@ ActionMailer::Base.smtp_settings = {
     user_name: 'SMTP_Injection',
     password: ENV['SPARKPOST_API_KEY'],
     address: ENV['SPARKPOST_SMTP_HOST'],
-    port: ENV['SPARKPOST_SMTP_PORT',
+    port: ENV['SPARKPOST_SMTP_PORT'],
     enable_starttls_auto: true,
     format: :html,
     from: 'mailer@yourawesomeapp.com'
 }
 # ActionMailer::Base.default_url_options[:host] = "affordablehousingapp.com"
+
