3b1871bcc749d7d2aeaa7fe86f6288ee9fced7eb
Marlon J. Frausto <marlonjfrausto@gmail.com>
2016-02-11 19:25:33 -0800

    FIX: added stuff

---
+++ config/initializers/email_settings.rb
@@ -0,0 +1,9 @@
+ActionMailer::Base.smtp_settings = {
+    user_name: 'SMTP_Injection',
+    password: 'A7683b93d766bdeace1ee925d8546746390841bd0',
+    address: 'smtp.sparkpostmail.com',
+    port: 587,
+    enable_starttls_auto: true,
+    format: :html,
+    from: 'marlon@foremanit.com'
+}
