2cd633f5b02eecc2374d626e5cbf8d19add50b30
Marlon J. Frausto <marlonjfrausto@gmail.com>
2016-02-11 20:22:45 -0800

    FIX: added some stuff for configuration

--- config/initializers/email_settings.rb
+++ config/initializers/email_settings.rb
@@ -1,3 +1,5 @@
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.smtp_settings = {
     user_name: 'SMTP_Injection',
     password: 'A7683b93d766bdeace1ee925d8546746390841bd0',
@@ -6,5 +8,7 @@ ActionMailer::Base.smtp_settings = {
     enable_starttls_auto: true,
     format: :html,
     from: 'marlon@affordablehousingapp.com',
+    :authentication => :plain,
     :openssl_verify_mode => 'none'
 }
+ActionMailer::Base.default_url_options[:host] = "qmhs.herokuapp.com"
