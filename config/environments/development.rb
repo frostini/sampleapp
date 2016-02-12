067a82c409502e4e53d003e1d0b1cdff11381903
Marlon J. Frausto <marlonjfrausto@gmail.com>
2016-02-11 20:40:12 -0800

    testing in development as well

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,5 +37,27 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
 
   # Raises error for missing translations
+
+
+
   # config.action_view.raise_on_missing_translations = true
+
+
+
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.perform_deliveries = true
+config.action_mailer.smtp_settings = {
+    user_name: 'SMTP_Injection',
+    password: 'A7683b93d766bdeace1ee925d8546746390841bd0',
+    address: 'smtp.sparkpostmail.com',
+    port: 587,
+    enable_starttls_auto: true,
+    format: :html,
+    from: 'marlon@affordablehousingapp.com'
+    # :authentication => :plain,
+    # :openssl_verify_mode => 'none'
+}
+config.action_mailer.default_url_options[:host] = "affordablehousingapp.com"
+
+
 end
