b749a68400f859561c6fd554051ee775dc38e4c8
Marlon J. Frausto <marlonjfrausto@gmail.com>
2016-02-11 21:59:03 -0800

    FIX: added the initializer for email script. lets see what it does

--- config/initializers/email_settings.rb
+++ config/initializers/email_settings.rb
@@ -11,7 +11,6 @@
 #     :authentication => :plain,
 #     :openssl_verify_mode => 'none'
 # }
-# ActionMailer::Base.default_url_options[:host] = "affordablehousingapp.com"
 
 # config.action_mailer.delivery_method = :smtp
 # config.action_mailer.perform_deliveries = true
@@ -27,3 +26,16 @@
     # :openssl_verify_mode => 'none'
 # }
 # config.action_mailer.default_url_options[:host] = "affordablehousingapp.com"
+
+
+
+ActionMailer::Base.smtp_settings = {
+    user_name: 'SMTP_Injection',
+    password: ENV['SPARKPOST_API_KEY'],
+    address: ENV['SPARKPOST_SMTP_HOST'],
+    port: ENV['SPARKPOST_SMTP_PORT',
+    enable_starttls_auto: true,
+    format: :html,
+    from: 'mailer@yourawesomeapp.com'
+}
+# ActionMailer::Base.default_url_options[:host] = "affordablehousingapp.com"
