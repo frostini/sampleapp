3a1e04c90e74ee2ac38750d11007ee10fa2a9dda
Marlon J. Frausto <marlonjfrausto@gmail.com>
2016-02-18 20:20:40 -0800

    FIX: styled the flash messages. customizes the flash message text.

--- app/controllers/inquiry_controller.rb
+++ app/controllers/inquiry_controller.rb
@@ -6,10 +6,10 @@ class InquiryController < ApplicationController
       if @inquiry.save
         ContactMailer.new_inquiry_email(@inquiry).deliver_now
         ContactMailer.welcome_email(@inquiry).deliver_now
-        format.html { redirect_to :root, notice: 'assistant was successfully created.' }
+        format.html { redirect_to :root, notice: 'Thank you for reaching out. We will be contacting you shortly.' }
         format.json { render :root, status: :created, location: :root }
       else
-        format.html { render :root }
+        format.html { render :root,  notice: 'There was an error processing your request. Please try again at a later time.'}
         format.json { render json: @inquiry.errors, status: :unprocessable_entity }
       end
     end
